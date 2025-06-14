a2d5a9fd2ca48916a4118d6fc8625194fed993ea
gozdesglt <sagalticigozde85@gmail.com>
2025-06-14 14:24:42 +0300

    Sinema rezervasyon web sitesi arayüzü
    Sinema rezervasyon web sitesi ara yüzüdür. HTML,CSS,JS kullanılmıştır.

---
+++ main.js
@@ -0,0 +1,22 @@
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+    1280: { slidesPerView: 4 }
+  }
+});
+
+AOS.init();
